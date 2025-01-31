0fda7afba9cef73a97203ed9046656ba95276739
Tiago Sabadini <sabadini.tiago@gmail.com>
2025-01-31 08:39:54 -0300

    feat: css configuration

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,42 +14,56 @@ const Header = () => {
         setDrawer(false);
     }, [currentPath]);
 
-    return (
-        <nav className="bg-primary py-3 sm:py-4 flex justify-start items-center px-4 sm:px-0 sm:justify-center">
-            <button className="sm:hidden mr-3">
-                <MdMenu size={24} onClick={() => setDrawer(true)} />
-            </button>
+    useEffect(() => {
+        const handle = (e: KeyboardEvent) =>{
+            if(e.key === 'Escape'){
+                setDrawer(false);
+            }
+        };
 
-            <div 
-                data-open={drawer}
-                onClick={() => setDrawer(false)}
-                className="fixed left-0 top-0 right-0 h-full bg-gradient-to-r from-background transition-transform data-[open=false]:-translate-x-full">
-                <ul className="flex-col sm:hidden p-4 gap-5 bg-background w-60 h-full" onClick={(e) => e.stopPropagation()}>
-                    <li><Link data-active={currentPath === "/"} className="data-[active=true]:underline" href="/">Home</Link></li>
-                    <li><Link data-active={currentPath === "/cursos"} className="data-[active=true]:underline" href="/cursos">Cursos</Link></li>
+        window.addEventListener('keydown', handle);
+        return () => window.removeEventListener('keydown', handle);
+    }, []);
+
+    return (
+        <>
+            <nav className="bg-primary py-3 fixed left-0 top-0 right-0 sm:py-4 flex justify-start items-center px-4 sm:px-0 sm:justify-center">
+                <button className="sm:hidden mr-3">
+                    <MdMenu size={24} onClick={() => setDrawer(true)} />
+                </button>
+
+                <div 
+                    data-open={drawer}
+                    onClick={() => setDrawer(false)}
+                    className="sm:hidden fixed left-0 top-0 right-0 h-full bg-gradient-to-r from-background transition-transform data-[open=false]:-translate-x-full">
+                    <ul className="flex-col sm:hidden p-4 gap-5 bg-background w-60 h-full" onClick={(e) => e.stopPropagation()}>
+                        <li><Link data-active={currentPath === "/"} className="data-[active=true]:underline" href="/">Home</Link></li>
+                        <li><Link data-active={currentPath === "/cursos"} className="data-[active=true]:underline" href="/cursos">Cursos</Link></li>
+                        <li>
+                            <Link  href="/" className="flex items-center gap-1">
+                                Blog
+                                <MdOutlineOpenInNew />
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <Link href="/" className="border-2 rounded-md py-2 px-3 no-underline hover:no-underline mr-4">CODARSE</Link>
+                <ul className="p-4 gap-5 hidden sm:inline-flex sm:ml-2">
+                    <li><Link data-active={currentPath === "/"} className="data-[active=true]:underline outline-offset-4" href="/">Home</Link></li>
+                    <li><Link data-active={currentPath === "/cursos"} className="data-[active=true]:underline outline-offset-4" href="/cursos">Cursos</Link></li>
                     <li>
-                        <Link  href="/" className="flex items-center gap-1">
+                        <Link  href="/" className="flex items-center gap-1 outline-offset-4">
                             Blog
                             <MdOutlineOpenInNew />
                         </Link>
                     </li>
                 </ul>
-            </div>
-
-
-            <Link href="/" className="border-2 rounded-md py-2 px-3 no-underline hover:no-underline mr-4">CODARSE</Link>
-            <ul className="p-4 gap-5 hidden sm:inline-flex sm:ml-2">
-                <li><Link data-active={currentPath === "/"} className="data-[active=true]:underline" href="/">Home</Link></li>
-                <li><Link data-active={currentPath === "/cursos"} className="data-[active=true]:underline" href="/cursos">Cursos</Link></li>
-                <li>
-                    <Link  href="/" className="flex items-center gap-1">
-                        Blog
-                        <MdOutlineOpenInNew />
-                    </Link>
-                </li>
-            </ul>
-            <h1 className="sm:hidden">{title}</h1>
-        </nav>
+                <h1 className="sm:hidden line-clamp-1">{title}</h1>
+            </nav>
+            <div className="h-[72px] sm:h-[92px]" />
+        </>
     );
 };
 
