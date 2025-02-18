45c7053c54c6d3c53d604c59c9f1e731b3e753df
Tiago Sabadini <sabadini.tiago@gmail.com>
2025-02-18 07:54:05 -0300

    feat: configured scroll control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -176,6 +176,60 @@ export default function PageHome() {
                             CODARSE - Introdução e apresentação do projeto
   
                             #typescript #cursotypescript #javascript #cursodejavascript #reactetypescript #learningtypescript #learningjavascript` 
+                    },
+                    {
+                      href: "/cursos/123",
+                      title: "Curso de API Rest, Node e Typescript",
+                      image: "https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg",
+                      description: `
+                        Na primeira aula do curso de API Rest em Node e Typescript vamos ver juntos o escopo do curso e se esse curso faz sentido para você ou não. Quero te apresentar as tecnologias e conceitos que vamos usar no projeto que será desenvolvido ao longo do curso, ainda vamos entender como serão  criados os testes que usaremos para garantir a qualidade do código e no final vamos ver o frontend que eu vou te entregar de brinde para usar com esse backend.
+  
+  
+                            Links mencionados no vídeo:
+                            📖 Repositório do projeto - https://github.com/lvsouza/youtube-cu...
+                            🗨️ Discord -
+  
+                            / discord
+  
+                            Outros cursos no canal:
+                            🎩 Figma para DEVs -
+  
+                            • Figma para DEVs: #00 - Apresentação d...
+                            ☪️ Curso de estilização no React -
+  
+                            • Estilização no React: Aula 00 - Curso...
+                            ⭐️ Curso de React com typescript -
+  
+                            • Curso de react: Aula 01 - Começando c...
+                            👑 React, Material UI 5 e Typescript -
+  
+                            • React, Material UI 5 e Typescript: #0...
+                            🏆 Curso de API Rest, Node e Typescript -
+  
+                            • API Rest, Node e Typescript: #00 - Ap...
+  
+                            Livros recomendados:
+                            📘 Atomic design - https://amzn.to/48QZM5L
+                            📘 Código limpo - https://amzn.to/3tVMtl0
+                            📘 Arquitetura limpa - https://amzn.to/47FfH5q
+                            📘 Migrando sistemas monolíticos - https://amzn.to/4aU0mB7
+  
+                            Conteúdo:
+                            0:00 | Introdução
+                            0:27 | Apresentação das tecnologias usadas no curso
+                            2:20 | Pra quem é o curso?
+                            4:30 | Sobre a API Rest em Node JS que vamos desenvolver
+                            5:32 | Qual a proposta do curso?
+                            7:24 | A cereja do bolo
+                            8:44 | Outros cursos do canal
+                            10:00 | Vendo como ficou o projeto
+                            10:48 | Testando no insominia
+                            15:30 | Apresentando o frontend de testes
+                            19:50 | Considerações finais
+  
+                            CODARSE - Introdução e apresentação do projeto
+  
+                            #typescript #cursotypescript #javascript #cursodejavascript #reactetypescript #learningtypescript #learningjavascript` 
                     }
                   ]
                 }
--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { Card, ICardProps } from "../card/Card";
+import { UIEvent, useRef, useState } from "react";
 
 
 interface ISectionProps {
@@ -9,11 +13,34 @@ interface ISectionProps {
 
 export const Section = ({title, variant, items}: ISectionProps) => {
 
+    const scrollLeft = useRef<HTMLUListElement>(null);
+    const [scrollAt, setScrollAt] = useState<'start' | 'middle' | 'end'>('start');
+
+    const handleSetScroll = (scroll: number) => {
+        const currentScrollLeft = scrollLeft.current?.scrollLeft || 0;
+        scrollLeft.current?.scrollTo({ behavior: 'smooth', left: currentScrollLeft + scroll });
+    };
+
+    const handleScroll = (event: UIEvent<HTMLUListElement>) => {
+        const scrollWidth = event.currentTarget.scrollWidth;
+        const clientWidth = event.currentTarget.clientWidth;
+        const scrollLeft = event.currentTarget.scrollLeft;
+        if(scrollLeft === 0){
+            setScrollAt('start');
+        }else if((scrollWidth - clientWidth) == scrollLeft){
+            setScrollAt('end');
+        }else{
+            setScrollAt('middle');
+        }
+    };
+    
     return (
         <section className="flex flex-col gap-4 p-4">
             <h2 className="font-bold text-lg">{title}</h2>
 
-            <ul data-variant={variant}
+            <ul ref={scrollLeft} 
+                data-variant={variant}
+                onScroll={handleScroll}
                 className="
                     grid gap-4 
                     grid-cols-1
@@ -23,6 +50,16 @@ export const Section = ({title, variant, items}: ISectionProps) => {
                     data-[variant=h-list]:sm:grid-flow-col
                     data-[variant=h-list]:sm:overflow-scroll
                     ">
+
+                        {
+                            (variant == "h-list") ? 
+                                <button 
+                                    disabled={scrollAt == 'start'}
+                                    onClick={() => handleSetScroll(-300)}
+                                className="w-14 h-14 bg-primary rounded-full flex justify-center items-center my-auto sticky left-0 -ml-14 transition-opacity disabled:opacity-0">
+                                    <MdKeyboardArrowLeft size={32} />
+                                </button> : ""                            
+                        }
 
                         {items.map(
                             (item, index) => (
@@ -35,6 +72,17 @@ export const Section = ({title, variant, items}: ISectionProps) => {
                                 </li>
                             )
                             )}
+
+
+                        {
+                            (variant == "h-list") ? 
+                            <button 
+                                disabled={scrollAt == 'end'}
+                                onClick={() => handleSetScroll(300)}
+                                className="w-14 h-14 bg-primary rounded-full flex justify-center items-center my-auto sticky right-0 -ml-14 transition-opacity disabled:opacity-0">
+                                <MdKeyboardArrowRight size={32} />
+                            </button> : ""
+                        }
                     
             </ul>
         </section>
